e500d1ff96cebc2475ba44d15a922e7c114901bd
insuranceadvisornearme <insuranceadvisornearme@gmail.com>
2025-08-09 03:42:19 +0530

    Update settings.py

--- insuranceadvisor/settings.py
+++ insuranceadvisor/settings.py
@@ -102,6 +102,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
