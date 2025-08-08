9f388c432e5c5b6f93fcfe52c949864204797f06
insuranceadvisornearme <insuranceadvisornearme@gmail.com>
2025-08-09 02:48:07 +0530

    Update settings.py

--- insuranceadvisor/settings.py
+++ insuranceadvisor/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key-here'
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
